50d34d7c286184909e53e4b4b68d2dea43f89ce2
muhammad.asher <Asher@321>
2026-04-17 18:07:27 +0500

    Created water model

---
+++ lib/models/water_model.dart
@@ -0,0 +1,23 @@
+class Water {
+
+final String? id;
+final double amount;
+final String unit;
+final DateTime dateTime;
+
+  Water({this.id, required this.amount, required this.unit, required this.dateTime});
+
+
+factory Water.fromJson(Map<String, dynamic> json, String id){
+  return Water(id: id, amount: json["amount"], unit: json["unit"], dateTime: DateTime.now());
+}
+
+Map<String, dynamic> toJson(){
+  return{
+    "amount": amount,
+    "dateTime": DateTime.now(),
+    "unit": unit
+  };
+}
+
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +11,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final amountController = TextEditingController();
+
+  void savewater(String amount) async {
+    final url = Uri.https(
+      "water-intaker-1b921-default-rtdb.asia-southeast1.firebasedatabase.app",
+      "water.json",
+    );
+
+    var response = await http.post(
+      url,
+      headers: {"Content-Type": "Application/Json"},
+      body: json.encode({
+        "amount": double.parse(amount),
+        "unit" : "ml",
+        "dateTime": DateTime.now().toString()
+      }),
+    );
+
+    if(response.statusCode == 200){
+      print("api success");
+
+    }else{
+      print("error calling api");
+    }
+
+  }
+
   void waterDialog() {
     showDialog(
       context: context,
@@ -21,39 +51,33 @@ class _HomePageState extends State<HomePage> {
               Text("Add water to your daily life"),
               SizedBox(height: 15),
               TextField(
+                keyboardType: TextInputType.number,
+                controller: amountController,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   label: Text("Amount"),
                 ),
               ),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Cancel",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Save",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
             ],
           ),
+          actions: [
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                "Cancel",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                savewater(amountController.text.trim().toString());
+              },
+              child: Text(
+                "Save",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
         );
       },
     );
