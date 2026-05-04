065e1232bd20058ad57faad4e43cd2c570b8b8fa
muhammad.asher <Asher@321>
2026-05-04 13:13:50 +0500

    added bargrapf bottom custom widget

--- lib/bars/bar_graph.dart
+++ lib/bars/bar_graph.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -49,29 +51,75 @@ class BarGraph extends StatelessWidget {
           show: true,
           topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
           leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-          rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false))
+          rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: getBottomTitles,
+            ),
+          ),
         ),
-        
+
         barGroups: barData.bardata.map((data) {
           return BarChartGroupData(
             x: data.x,
-            barRods: [BarChartRodData(toY: data.y,
-            width: 20,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(6),
-              topRight: Radius.circular(6),
-              
-            ),
-            backDrawRodData: BackgroundBarChartRodData(
-              show: true,
-              toY: maxY,
-              color: Colors.grey[300]
-            ),
-            color: CupertinoColors.activeGreen),
+            barRods: [
+              BarChartRodData(
+                toY: data.y,
+                width: 20,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(6),
+                  topRight: Radius.circular(6),
+                ),
+                backDrawRodData: BackgroundBarChartRodData(
+                  show: true,
+                  toY: maxY,
+                  color: Colors.grey[300],
+                ),
+                color: CupertinoColors.activeGreen,
+              ),
             ],
           );
         }).toList(),
       ),
     );
+  }
+
+  Widget getBottomTitles(double value, TitleMeta meta) {
+    const TextStyle style = TextStyle(
+      color: Colors.blue,
+      fontWeight: FontWeight.bold,
+    );
+
+    final Text text;
+
+    switch (value.toInt()) {
+      case 0:
+        text = Text("S", style: style);
+        break;
+      case 1:
+        text = Text("M", style: style);
+        break;
+      case 2:
+        text = Text("T", style: style);
+        break;
+      case 3:
+        text = Text("W", style: style);
+        break;
+      case 4:
+        text = Text("T", style: style);
+        break;
+      case 5:
+        text = Text("F", style: style);
+        break;
+      case 6:
+        text = Text("S", style: style);
+        break;
+      default:
+       text = Text("");
+        break;
+    }
+
+    return SideTitleWidget(child: text, meta: meta, space: 4,);
   }
 }
